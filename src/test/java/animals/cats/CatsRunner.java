0b32374de11544b97f5aeefd42224f5afd0da2d9
36680 <August2k17>
2018-09-25 14:37:16 +0100

    refactored feature file and added build file

--- src/test/java/animals/cats/CatsRunner.java
+++ src/test/java/animals/cats/CatsRunner.java
@@ -4,10 +4,7 @@ import org.junit.runner.RunWith;
 
 import com.intuit.karate.junit4.Karate;
 
-import cucumber.api.CucumberOptions;
-
 @RunWith(Karate.class)
-@CucumberOptions(features = "classpath:animals/cats")
 public class CatsRunner {
 
 }
